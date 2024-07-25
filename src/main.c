7c3413c375e66a755c79be1c83c35d6c0f59dbf8
Shaun Pearce <shaun@kloudimi.com>
2024-07-26 00:40:35 +0200

    feat: tokens in linked list

--- src/main.c
+++ src/main.c
@@ -1,14 +1,84 @@
+/*
+ * This compiler compiles a language know as Funky.
+ * That is all.
+ */
 #include "allheaders.h"
-#include <string.h>
 
 const char *comment = "#";
 const char *whitespace = " \r\n";
-const char *delimiter = " \n\r";
-
+const char *delimiter = " \n\r,():";
+/*
+ */
+#define nonep(node) ((node).type == NODE_TYPE_NONE)
+#define integerp(node) ((node).type == NODE_TYPE_INTEGER)
+/*
+ */
+typedef struct Token {
+  char *beginning;
+  char *end;
+  struct Token *next;
+} Token;
+/*
+ */
+Token *token_create() {
+  Token *token = malloc(sizeof(Token));
+  assert(token && "Could not allocate Token");
+  memset(token, 0, sizeof(Token));
+  return token;
+}
+/*
+ */
+void print_tokens(Token *root) {
+  size_t count = 1;
+  while(root){
+    // FIXME remove inf loop checker
+    if(count > 10000) { break; }
+    printf("Token %zu: ", count);
+    if (root->beginning && root->end){
+      printf("%.*s", root->end - root->beginning, root->beginning);
+    }
+    putchar('\n');
+    root = root->next;
+    count++;
+  }
+}
+/*
+ */
+typedef long long integer_t;
+/*
+ */
+typedef struct Node {
+  enum NodeType {
+    NODE_TYPE_NONE = 0,
+    NODE_TYPE_INTEGER,
+    NODE_TYPE_PROGRAM,
+    NODE_TYPE_MAX,
+  } type;
+  union NodeValue {
+    integer_t integer;
+  } value;
+  struct Node **children;
+} Node;
+/*
+ */
+typedef struct Binding {
+  char *id;
+  Node *value;
+  struct Binding *next;
+} Binding;
+/*
+ */
+typedef struct Environment {
+  struct Environment *parent;
+  Binding *bind;
+} Environment;
+/*
+ */
 void print_usage(char **argv) {
   printf("Usage: %s <path_to_source>\n", argv[0]);
 }
-
+/*
+ */
 long file_size(FILE *file) {
   if (!file) {
     return 0;
@@ -26,7 +96,8 @@ long file_size(FILE *file) {
   }
   return out;
 }
-
+/*
+ */
 char *file_contents(char *path) {
   FILE *file = fopen(path, "r");
   if (!file) {
@@ -61,7 +132,6 @@ char *file_contents(char *path) {
   return contents;
 }
 /*
- *
  */
 typedef struct Error {
   enum ErrorType {
@@ -71,17 +141,21 @@ typedef struct Error {
     ERROR_SYNTAX,
     ERROR_GENERIC,
     ERROR_TODO,
+    ERROR_MAX,
   } type;
   char *message;
 } Error;
-
+/*
+ */
 Error ok = {ERROR_NONE, NULL};
-
+/*
+ */
 void print_error(Error err) {
   if (err.type == ERROR_NONE) {
     return;
   }
   printf("ERROR: ");
+  assert(ERROR_MAX == 6);
   switch (err.type) {
   case ERROR_TODO:
     printf("TODO (not implemented)");
@@ -107,41 +181,67 @@ void print_error(Error err) {
     printf("     : %s", err.message);
   }
 }
-
-#define ERROR_CREATE(n, t, msg) Error(n) = {(t), (msg)}
+/*
+ */
 #define ERROR_PREP(n, t, msg)                                                  \
   (n).type = (t);                                                              \
   (n).message = (msg)
-
-Error lex(char *source, char **beg, char **end) {
+/*
+ */
+Error lex(char *source, Token *token) {
   Error err = ok;
-  if (!source || !beg || !end) {
+  if (!source || !token) {
     ERROR_PREP(err, ERROR_ARGUMENTS, "Source cannot be empty");
     return err;
   }
-  //
-  *beg = source;
-  *beg += strspn(*beg, whitespace);
-  *end = *beg;
-  *end += strcspn(*beg, delimiter);
+
+  token->beginning = source;
+  token->beginning += strspn(token->beginning, whitespace);
+  token->end = token->beginning;
+  if (*(token->end) == '\0'){ return err; }
+  token->end += strcspn(token->beginning, delimiter);
+  if(token->end == token->beginning) {
+    token->end += 1;
+  }
+
   return err;
 }
-
-Error parse_source(char *source) {
+/*
+ */
+Error parse_source(char *source, Node *environment) {
   Error err = ok;
-  char *beg = source;
-  char *end = source;
+  Token *tokens = NULL;
+  Token *tokens_iter = tokens;
+
+  Token current_token;
+  current_token.next = NULL;
+  current_token.beginning = source;
+  current_token.end = source;
+
   // This is cursed C.. beware
-  while ((err = lex(end, &beg, &end)).type == ERROR_NONE) {
-    if (end - beg == 0) {
+  while ((err = lex(current_token.end, &current_token)).type == ERROR_NONE) {
+    if (current_token.end - current_token.beginning == 0) {
       break;
     }
-    printf("lexed: %.*s\n", end - beg, beg);
+    // Prepend to linked list
+    if(tokens){
+      tokens_iter->next = token_create();
+      memcpy(tokens_iter->next, &current_token, sizeof(Token));
+      tokens_iter = tokens_iter->next;
+    } else {
+      tokens = token_create();
+      memcpy(tokens, &current_token, sizeof(Token));
+      tokens_iter = tokens;
+    }
   }
+
+  print_tokens(tokens);
+
   return err;
 }
-
 /*
+ *
+ * Go Baby, Go!
  *
  */
 int main(int argc, char **argv) {
@@ -153,8 +253,8 @@ int main(int argc, char **argv) {
   char *path = argv[1];
   char *contents = file_contents(path);
   if (contents) {
-    // printf("Contents of %s\n---\n%s---\n", path, contents);
-    Error err = parse_source(contents);
+    Node env;
+    Error err = parse_source(contents, &env);
     free(contents);
   }
   print_error(err);
